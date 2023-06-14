27bdb182f5b08940f8aeea7997fcec52c2445521
wquf <97148865+wquf@users.noreply.github.com>
2023-06-14 19:48:34 +0900

    Cleaned whole code again

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ import random
 import webview
 import threading
 
-first = True
-window = None
+file     = sys.argv[1]
+first    = True
+window   = None
+current  = None
 previous = None
-current = None
-file = sys.argv[1]
 
 def update():
     global previous
@@ -20,10 +20,10 @@ def update():
         elif not first:
             break
 
-        with open(file, 'r', encoding='UTF-8') as source:
-            content = source.read()
+        soruce = open(file, 'r', encoding='UTF-8')
+        content = source.read()
         source.close()
-
+        
         if previous != content:
             previous = content
             window.evaluate_js('window.location.reload()')
@@ -33,18 +33,21 @@ def update():
         
 @retry.retry()
 def deploy():
-    def process():
+    def run():
         global port
         port = random.randint(8000, 19000)
 
         app = flask.Flask(__name__)
-        app.route('/')(lambda: previous)
+        index = app.route('/')
+        index(lambda: previous)
         return app.run('0.0.0.0', port)
     
-    thread = threading.Thread(target=process, daemon=True)
+    thread = threading.Thread(target=run, daemon=True)
     return thread.start()
 
 deploy() 
+
 window = webview.create_window('Live HTML Viewer', url=f'http://localhost:{port}')
 window.events.closed += lambda: globals().update({'window': None, 'first': False})
+
 webview.start(func=update)
